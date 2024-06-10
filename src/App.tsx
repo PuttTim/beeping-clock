22e8304ea360604d01d2cf72a6959a839e3ae63e
PuttTim <yannaputt@gmail.com>
2024-06-10 12:04:03 +0800

    beep beep

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,79 @@ const transformToMorseCode = (time: string) => {
 }
 
 function App() {
+    const DOT_TIME = 60
+    const DASH_TIME = DOT_TIME * 3
+    const SYMBOL_BREAK = DOT_TIME
+    const LETTER_BREAK = DOT_TIME * 3
+    const WORD_BREAK = DOT_TIME * 7
+
+    const audioCtx = new AudioContext()
+    const oscNode = audioCtx.createOscillator()
+    const gainNode = audioCtx.createGain()
+
+    oscNode.type = "sine"
+    oscNode.frequency.value = 800
+    gainNode.gain.value = 0
+
+    oscNode.connect(gainNode)
+    gainNode.connect(audioCtx.destination)
+    oscNode.start()
+
+    const startAudio = () => {
+        gainNode.gain.setTargetAtTime(0.1, 0, 0.001)
+    }
+
+    const stopAudio = () => {
+        gainNode.gain.setTargetAtTime(0, 0, 0.001)
+    }
+
+    const sleep = (ms: number) => {
+        return new Promise(resolve => setTimeout(resolve, ms))
+    }
+
+    const playDot = async () => {
+        // if (currentPlayCounter != playCounter) { return; }
+        startAudio()
+        await sleep(DOT_TIME)
+        stopAudio()
+    }
+
+    const playDash = async () => {
+        // if (currentPlayCounter != playCounter) { return; }
+        startAudio()
+        await sleep(DASH_TIME)
+        stopAudio()
+    }
+
     const [time, setTime] = useState(transformToMorseCode("00:00:00"))
+    const [frozenTime, setFrozenTime] = useState("")
+    const [currentlyPlaying, setCurrentlyPlaying] = useState(false)
+
+    const fireBeeps = async () => {
+        if (currentlyPlaying) {
+            return
+        }
+
+        setCurrentlyPlaying(true)
+
+        const currentTime = Object.values(time)
+
+        for (let i = 0; i < 3; i++) {
+            const b = currentTime[i].split("")
+            for (let j = 0; j < b.length; j++) {
+                if (b[i] === ".") {
+                    await playDot()
+                } else if (b[i] === "-") {
+                    await playDash()
+                }
+                await sleep(SYMBOL_BREAK)
+            }
+            await sleep(LETTER_BREAK)
+        }
+
+        setFrozenTime("")
+        setCurrentlyPlaying(false)
+    }
 
     const loopyLoop = setInterval(() => {
         setTime(
@@ -66,6 +138,18 @@ function App() {
                     <h1 className="text">Seconds {time.s}</h1>
                 </code>
             </div>
+            <button
+                onClick={() => {
+                    setFrozenTime(
+                        time.hour + "  :  " + time.min + "  :  " + time.s,
+                    )
+                    fireBeeps()
+                }}
+                className="btn">
+                Play Current Beeps
+            </button>
+
+            <h1 className="text">{frozenTime ?? ""}</h1>
         </div>
     )
 }
